4c3891789dca5fd43a7bb68e9e953beec609f093
Vasco Santos <santos.vasco10@gmail.com>
2023-06-23 15:26:01 +0200

    feat: redirect to roundabout (#4)

--- src/worker.js
+++ src/worker.js
@@ -1,6 +1,5 @@
 /* eslint-env worker */
 import { HttpError } from './errors.js'
-import { toR2Range } from './r2.js'
 
 export default {
   async fetch (request, env) {
@@ -19,49 +18,16 @@ export default {
 
 /**
  * @param {Request} request
- * @param {{ DAGCARGO: any }} env
+ * @param {{ DAGCARGO: any, ROUNDABOUT_API_URL: string, DAGCARGO_BUCKET_NAME: string }} env
  */
 async function handler (request, env) {
   const url = new URL(request.url)
   const key = url.pathname.slice(1)
 
   if (request.method === 'GET') {
-    /** @type {import('./r2').R2Range|undefined} */
-    let range
-    if (request.headers.has('range')) {
-      try {
-        range = toR2Range(request.headers.get('range'))
-      } catch (err) {
-        throw new HttpError('Invalid Range', { status: 400, cause: err })
-      }
-    }
-
-    const object = await env.DAGCARGO.get(key, { range })
-    if (!object || !object.body) {
-      throw new HttpError('Object Not Found', { status: 404 })
-    }
-
-    const status = range ? 206 : 200
-    const headers = new Headers()
-    object.writeHttpMetadata(headers)
-    headers.set('etag', object.httpEtag)
-
-    if (range) {
-      let first, last
-      if (range.suffix != null) {
-        first = object.size - range.suffix
-        last = object.size - 1
-      } else {
-        first = range.offset || 0
-        last = range.length != null ? first + range.length - 1 : object.size - 1
-      }
-      headers.set('content-range', `bytes ${first}-${last}/${object.size}`)
-      headers.set('content-length', last - first + 1)
-    } else {
-      headers.set('content-length', object.size)
-    }
-
-    return new Response(object.body, { headers, status })
+    const url = new URL(key, env.ROUNDABOUT_API_URL)
+    url.searchParams.set('bucket', env.DAGCARGO_BUCKET_NAME)
+    return Response.redirect(url)
   }
 
   if (request.method === 'HEAD') {
